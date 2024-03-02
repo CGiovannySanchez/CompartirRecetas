8f1ac5f6feb2670bf637758547beea22e01af271
CGiovannySanchez <giovanny-sn@hotmail.com>
2024-03-01 23:31:30 -0500

    Cracion de proyecto y aplication properties

---
+++ src/main/java/com/giovanni/CompartirRecetasApplication.java
@@ -0,0 +1,13 @@
+package com.giovanni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompartirRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompartirRecetasApplication.class, args);
+	}
+
+}
